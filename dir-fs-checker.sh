ccf090c3f030aa2a1d08830f3a7d72e2d3d9c3b2
Erwyne Santos <80817295+erwynesantos@users.noreply.github.com>
2021-03-19 21:56:10 +0800

    Create dir-fs-checker.sh

---
+++ dir-fs-checker.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+# Checks used space on filesystems and reports any over specified percentage
+
+# Warn when percentage used space reaches max:
+max="95"
+
+# Paths to file systems to check: (e.g. /media/mount_point /mnt/mount_point)
+filesystems=(/media/foo /media/bar /mnt/foo)
+
+for fs in ${filesystems[*]}; do
+	#check if filesystem is actually mounted
+	if grep -qs "$fs" /proc/mounts; then
+		space=$(df -h | grep $fs | awk '{print $(NF-1)}' | sed 's/%//')
+		if [ "$space" -ge "$max" ]; then
+			echo "$fs is at "$space"%"
+		fi
+	fi
+done
